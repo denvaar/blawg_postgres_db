38229c2ee773438dda30d5e8c5c41fe72b72802d
Denver Smith <denverpsmith@gmail.com>
2019-09-01 09:12:36 -0600

    Add seed data
    - Add ExDoc
    - Change date published field type

--- config/test.exs
+++ config/test.exs
@@ -2,7 +2,7 @@ use Mix.Config
 
 config :blawg_postgres_db, BlawgPostgresDb.Repo,
   username: "postgres",
-  password: "postgres",
+  password: "",
   database: "blawg_postgres_db_test",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
--- lib/blawg_postgres_db.ex
+++ lib/blawg_postgres_db.ex
@@ -1,6 +1,4 @@
 defmodule BlawgPostgresDb do
-
-  @doc delegate_to: {BlawgPostgresDb.Articles, :create_article, 1}
   defdelegate create_article(params), to: BlawgPostgresDb.Articles
   defdelegate update_article(slug, params), to: BlawgPostgresDb.Articles
   defdelegate get_article(slug), to: BlawgPostgresDb.Articles
